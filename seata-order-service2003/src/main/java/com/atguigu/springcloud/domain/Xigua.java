b09146859d8fc6404b3432ae307bc49bc2536e3e
461280293 <59387564+461280293@users.noreply.github.com>
2021-01-11 16:05:41 +0800

    在线编辑

--- seata-order-service2003/src/main/java/com/atguigu/springcloud/domain/Xigua.java
+++ seata-order-service2003/src/main/java/com/atguigu/springcloud/domain/Xigua.java
@@ -4,5 +4,6 @@ public class Xigua {
     public static void main(String[] args) {
         System.out.println("西瓜视频.....");
         System.out.println("西瓜视频.....1");
+        System.out.println("在线编辑");
     }
 }
