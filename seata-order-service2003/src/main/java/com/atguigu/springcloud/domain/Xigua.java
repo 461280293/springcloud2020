9bf511e6dbd95c4014c514bde0e763c1a1dda203
46128 <461280293@qq.com>
2021-01-11 15:34:16 +0800

    添加西瓜视频文件2

--- seata-order-service2003/src/main/java/com/atguigu/springcloud/domain/Xigua.java
+++ seata-order-service2003/src/main/java/com/atguigu/springcloud/domain/Xigua.java
@@ -3,5 +3,6 @@ package com.atguigu.springcloud.domain;
 public class Xigua {
     public static void main(String[] args) {
         System.out.println("西瓜视频.....");
+        System.out.println("西瓜视频.....1");
     }
 }
